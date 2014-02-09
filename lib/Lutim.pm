d144db93107992a2979443298d8a95bf2e3dba63
Luc Didry <luc@didry.org>
2014-02-09 17:27:32 +0100

    First import after mojo generate app

---
+++ lib/Lutim.pm
@@ -0,0 +1,18 @@
+package Lutim;
+use Mojo::Base 'Mojolicious';
+
+# This method will run once at server start
+sub startup {
+  my $self = shift;
+
+  # Documentation browser under "/perldoc"
+  $self->plugin('PODRenderer');
+
+  # Router
+  my $r = $self->routes;
+
+  # Normal route to controller
+  $r->get('/')->to('example#welcome');
+}
+
+1;
