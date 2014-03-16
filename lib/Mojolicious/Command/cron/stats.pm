12e0bf093a631ff4e52fa700f234543389621ba5
Luc Didry <luc@didry.org>
2014-03-17 00:15:16 +0100

    Fix bug in stats date sorting

--- lib/Mojolicious/Command/cron/stats.pm
+++ lib/Mojolicious/Command/cron/stats.pm
@@ -36,9 +36,9 @@ sub run {
     }
 
     my $total = LutimModel::Lutim->count('WHERE path IS NOT NULL AND created_at < ?', $separation);
-    for my $year (sort keys %data) {
-        for my $month (sort keys %{$data{$year}}) {
-            for my $day (sort keys %{$data{$year}->{$month}}) {
+    for my $year (sort {$a <=> $b} keys %data) {
+        for my $month (sort {$a <=> $b} keys %{$data{$year}}) {
+            for my $day (sort {$a <=> $b} keys %{$data{$year}->{$month}}) {
                 $thead_tr->append_content('<th>'."$day/$month/$year".'</th>'."\n");
                 $tbody_tr->append_content('<td>'.$data{$year}->{$month}->{$day}.'</td>'."\n");
                 $total += $data{$year}->{$month}->{$day};
