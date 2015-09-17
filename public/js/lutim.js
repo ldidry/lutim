af9323365c4c804cd318a62cab953671ac6e5395
Luc Didry <luc@didry.org>
2015-09-17 21:47:37 +0200

    Move javascript which don't need templating to static js file

--- public/js/lutim.js
+++ public/js/lutim.js
@@ -1,3 +1,42 @@
+function addItem(item) {
+    var files = localStorage.getItem('images');
+    if (files === null) {
+        files = new Array();
+    } else {
+        files = JSON.parse(files);
+    }
+    files.push(item);
+    localStorage.setItem('images', JSON.stringify(files));
+}
+function share(url) {
+    new MozActivity({
+        name: 'share',
+        data: {
+            type: 'url',
+            number: 1,
+            url: url
+        }
+    });
+}
+function evaluateCopyAll() {
+    setTimeout(function() {
+        if ($('.view-link-input').length === 0) {
+            $('#copy-all').parent().remove();
+        }
+    }, 5);
+}
+window.onload = function() {
+    if (navigator.mozSetMessageHandler !== undefined) {
+        navigator.mozSetMessageHandler('activity', function handler(activityRequest) {
+            var activityName = activityRequest.source.name;
+            if (activityName == 'share') {
+                activity = activityRequest;
+                blob = activity.source.data.blobs[0];
+                fileUpload(blob);
+            }
+        });
+    }
+};
 $('document').ready(function() {
     $('.jsonly').show();
     $('.input-group-addon.jsonly').css('display', 'table-cell');
@@ -24,4 +63,17 @@ $('document').ready(function() {
             }
         }
     }
+    var firstview = ($('#first-view').prop('checked')) ? 1 : 0;
+    var deleteday = ($('#delete-day').prop('checked')) ? 1 : 0;
+
+    bindddz(firstview, deleteday);
+
+    $('#file-url-button').on('click', upload_url);
+    $('#lutim-file-url').keydown( function(e) {
+        var key = e.charCode ? e.charCode : e.keyCode ? e.keyCode : 0;
+        if(key == 13) {
+            e.preventDefault();
+            upload_url();
+        }
+    });
 });
