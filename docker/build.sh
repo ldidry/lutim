3dd1fdca56a173b20dd31eb6627b265d19ccb00a
Ploc <391-ploc@users.noreply.framagit.org>
2021-06-18 11:18:26 +0200

    chore: use imagemagick package from alpine
    Use imagemagick package from alpine instead of rebuilding it from source.

--- docker/build.sh
+++ docker/build.sh
@@ -2,10 +2,8 @@
 
 set -eu
 
-apk --update add perl libpq perl-crypt-rijndael perl-io-socket-ssl perl-net-ssleay su-exec shared-mime-info libressl
+apk --update add perl libpq perl-crypt-rijndael perl-io-socket-ssl perl-net-ssleay su-exec shared-mime-info libressl imagemagick imagemagick-perlmagick
 sh $(dirname $0)/install-dev-env.sh
-
-sh $(dirname $0)/install-imagemagick.sh
 
 cpan notest Carton Config::FromHash
 carton install --without test
